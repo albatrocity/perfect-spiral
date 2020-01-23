95825cb6d0b4d355a67cd695767aa1af7e440abd
Ross Brown <ross@cremalab.com>
2020-01-22 23:01:32 -0600

    adjusts order of songs

--- src/lib/sounds.js
+++ src/lib/sounds.js
@@ -1,10 +1,14 @@
 const sounds = [
   {
-    title: "Touchdown!",
+    title: "Touchdown",
     sprite: [313000, 50000],
     name: "touchdown1",
     full: true,
   },
+  { title: "First Down", sprite: [476000, 16000], name: "first-down" },
+  { title: "Offsides", sprite: [236000, 28000], name: "offsides" },
+  { title: "It's Good!", sprite: [193000, 15000], name: "its-good" },
+  { title: "No Good!", sprite: [214000, 16000], name: "no-good" },
   { title: "3rd-and-9", sprite: [1, 48000], name: "third-and-nine" },
   { title: "Generous Spot", sprite: [104000, 22000], name: "generous-spot" },
   { title: "Fumbling", sprite: [60000, 36000], name: "fumbling" },
@@ -13,21 +17,11 @@ const sounds = [
     sprite: [132200, 18000],
     name: "go-for-it",
   },
-  { title: "Interception!", sprite: [158000, 26000], name: "interception" },
-  { title: "It's Good!", sprite: [193000, 15000], name: "its-good" },
-  { title: "No Good!", sprite: [214000, 16000], name: "no-good" },
-  { title: "Offsides", sprite: [236000, 28000], name: "offsides" },
+  { title: "Interception", sprite: [158000, 26000], name: "interception" },
   { title: "Punt!", sprite: [280000, 13500], name: "punt" },
   { title: "Red Zone", sprite: [368000, 23000], name: "red-zone" },
   { title: "Sack", sprite: [404000, 24000], name: "sack" },
   { title: "Holding", sprite: [436000, 34000], name: "holding" },
-  { title: "First Down", sprite: [476000, 16000], name: "first-down" },
-  {
-    title: "Touchdown!",
-    sprite: [313000, 50000],
-    name: "touchdown",
-    full: true,
-  },
 ]
 
 export default sounds
