b7fb55b61b7fe1194b1a404e90858a64570e1c4f
Ross Brown <ross@cremalab.com>
2020-01-24 18:04:06 -0600

    adds webm files

--- src/lib/sounds.js
+++ src/lib/sounds.js
@@ -1,75 +1,115 @@
 const sounds = [
   {
     title: "Touchdown",
-    files: ["https://football-soundboard.s3.amazonaws.com/Touchdown.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/Touchdown.webm",
+      "https://football-soundboard.s3.amazonaws.com/Touchdown.mp3",
+    ],
     name: "touchdown1",
     full: true,
   },
   {
     title: "First Down",
-    files: ["https://football-soundboard.s3.amazonaws.com/First+Down.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/First+Down.webm",
+      "https://football-soundboard.s3.amazonaws.com/First+Down.mp3",
+    ],
     name: "first-down",
   },
   {
     title: "Offsides",
-    files: ["https://football-soundboard.s3.amazonaws.com/Offsides.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/Offsides.webm",
+      "https://football-soundboard.s3.amazonaws.com/Offsides.mp3",
+    ],
     name: "offsides",
   },
   {
     title: "It's Good!",
-    files: ["https://football-soundboard.s3.amazonaws.com/It_s+Good.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/It's+Good.webm",
+      "https://football-soundboard.s3.amazonaws.com/It_s+Good.mp3",
+    ],
     name: "its-good",
   },
   {
     title: "No Good!",
-    files: ["https://football-soundboard.s3.amazonaws.com/No+Good.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/No+Good.webm",
+      "https://football-soundboard.s3.amazonaws.com/No+Good.mp3",
+    ],
     name: "no-good",
   },
   {
     title: "3rd-and-9",
-    files: ["https://football-soundboard.s3.amazonaws.com/3rd+and+9.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/3rd+and+9.webm",
+      "https://football-soundboard.s3.amazonaws.com/3rd+and+9.mp3",
+    ],
     name: "third-and-nine",
   },
   {
     title: "Generous Spot",
-    files: ["https://football-soundboard.s3.amazonaws.com/Generous+Spot.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/Generous+Spot.webm",
+      "https://football-soundboard.s3.amazonaws.com/Generous+Spot.mp3",
+    ],
     name: "generous-spot",
   },
   {
     title: "Fumbling",
-    files: ["https://football-soundboard.s3.amazonaws.com/Fumbling.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/Fumbling.webm",
+      "https://football-soundboard.s3.amazonaws.com/Fumbling.mp3",
+    ],
     name: "fumbling",
   },
   {
     title: "Go For It (On 4th Down)",
     files: [
+      "https://football-soundboard.s3.amazonaws.com/Go+For+It+(On+the+4th+Down).webm",
       "https://football-soundboard.s3.amazonaws.com/Go+For+It+(On+the+4th+Down).mp3",
     ],
     name: "go-for-it",
   },
   {
     title: "Interception",
-    files: ["https://football-soundboard.s3.amazonaws.com/Interception.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/Interception.webm",
+      "https://football-soundboard.s3.amazonaws.com/Interception.mp3",
+    ],
     name: "interception",
   },
   {
     title: "Punt!",
-    files: ["https://football-soundboard.s3.amazonaws.com/Punt!.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/Punt!.webm",
+      "https://football-soundboard.s3.amazonaws.com/Punt!.mp3",
+    ],
     name: "punt",
   },
   {
     title: "Red Zone",
-    files: ["https://football-soundboard.s3.amazonaws.com/Red+Zone.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/Red+Zone.webm",
+      "https://football-soundboard.s3.amazonaws.com/Red+Zone.mp3",
+    ],
     name: "red-zone",
   },
   {
     title: "Sack",
-    files: ["https://football-soundboard.s3.amazonaws.com/Sack.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/Sack.webm",
+      "https://football-soundboard.s3.amazonaws.com/Sack.mp3",
+    ],
     name: "sack",
   },
   {
     title: "Holding",
-    files: ["https://football-soundboard.s3.amazonaws.com/Holding.mp3"],
+    files: [
+      "https://football-soundboard.s3.amazonaws.com/Holding.webm",
+      "https://football-soundboard.s3.amazonaws.com/Holding.mp3",
+    ],
     name: "holding",
   },
 ]
