8216de93d4cf5c9a989584de48b5eaa44c91025d
Ross Brown <ross@cremalab.com>
2020-01-28 21:41:59 -0600

    Adds opengraph description tag

--- src/components/seo.js
+++ src/components/seo.js
@@ -67,6 +67,10 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: `og:description`,
+          content: metaDescription,
+        },
       ].concat(meta)}
     />
   )
